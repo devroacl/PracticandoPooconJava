699832342fbce7e8e32ba67f3dcca5275da5d997
devroa <113223017+devroacl@users.noreply.github.com>
2025-08-18 09:50:01 -0400

    Primer ejercicio Practica Java

---
+++ New Folder/FarmaciaComunitaria/src/main/java/com/java/farmaciacomunitaria/FarmaciaComunitaria.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.java.farmaciacomunitaria;
+
+/**
+ *
+ * @author vina
+ */
+public class FarmaciaComunitaria {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+    
+    
+    
+    
+}
