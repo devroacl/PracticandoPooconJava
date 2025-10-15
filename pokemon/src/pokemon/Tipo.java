b2eace1b2817fccc961f5c90e8e9da10c7b6d858
devroa <roa.alejandra02@gmail.com>
2025-10-14 21:08:31 -0300

    Se agregaron clases pedidas por el ejercicio con sus metodos y atributos

---
+++ pokemon/src/pokemon/Tipo.java
@@ -0,0 +1,26 @@
+
+package pokemon;
+
+public class Tipo {
+     private String nombre;
+
+    public Tipo() {
+    }
+
+    public Tipo(String nombre) {
+        this.nombre = nombre;
+    }
+     
+     
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+     
+     
+     
+}
